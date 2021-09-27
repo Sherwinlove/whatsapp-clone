c438e4d39c26b387317e15863842d4f2bccc44e6
Sherwinlove <ilak0603@gmail.com>
2021-09-27 08:24:11 -0700

    fixed typos

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAcpJ0PLCt0DhwpJzOxbFcBeV3XRgO8vt0",
     authDomain: "whatsapp-clone-37573.firebaseapp.com",
@@ -7,3 +9,12 @@ const firebaseConfig = {
     appId: "1:386250939761:web:733878216ad124bf67aad0",
     measurementId: "G-LTL0JQW903"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
