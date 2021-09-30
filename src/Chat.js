51a63679082d86ba5b066009d791aa0a9ddf9cf3
Sherwinlove <ilak0603@gmail.com>
2021-09-29 23:32:55 -0700

    dynamic room name by Route

--- src/Chat.js
+++ src/Chat.js
@@ -1,63 +1,88 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
-import { Avatar, IconButton } from '@mui/material/';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { AttachFile, InsertEmoticon, MicNone, SearchOutlined } from '@material-ui/icons';
-import { useParams } from 'react-router-dom'
+import { Avatar, IconButton } from "@mui/material/";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {
+  AttachFile,
+  InsertEmoticon,
+  MicNone,
+  SearchOutlined,
+} from "@material-ui/icons";
+import { useParams } from "react-router-dom";
+import db from "./firebase";
 
 function Chat() {
-    const [seed, setSeed] = useState("");
-    const [input, setInput] = useState("");
-    const { roomId } = useParams();
+  const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
+  const { roomId } = useParams();
+  const [roomName, setRoomName] = useState("");
 
-    useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000))
-    }, []);
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        if (doc.id === roomId) {
+          setRoomName(doc.data().name);
+        }
+      });
+    });
+  }, [roomId]);
 
-    const sendMessage = (e) => {
-        e.preventDefault();
-        console.log("You typed...", input);
-        setInput("");
-    }
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
 
-    return (
-        <div className="chat">
-            <div className="chat__header">
-                <Avatar src={`https://avatars.dicebear.com/api/pixel-art/:${seed}.svg`}/>
-                <div className="chat__headerInfo">
-                    <h3>Room Name</h3>
-                    <p>Last seen at...</p>
-                </div>
-                <div className="chat__headerRight">
-                    <IconButton>
-                        <SearchOutlined />
-                    </IconButton>
-                    <IconButton>
-                        <AttachFile />
-                    </IconButton>
-                    <IconButton>
-                        <MoreVertIcon />
-                    </IconButton>
-                </div>
-            </div>
-            <div className="chat__body">
-                <p className={`chat__message ${true &&'chat__receiver'}`}>
-                <span className="chat__name">John Yasis</span>
-                Hey Guys
-                <span className="chat__timestamp">3:69pm</span>
-                </p>
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("You typed...", input);
+    setInput("");
+  };
 
-            </div>
-            <div className="chat__footer">
-                <InsertEmoticon />
-                <form> 
-                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message..." type="text" />
-                    <button onClick={sendMessage} type="submit">Send a  message...</button>
-                </form>
-                <MicNone />
-            </div>
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar
+          src={`https://avatars.dicebear.com/api/pixel-art/:${seed}.svg`}
+        />
+        <div className="chat__headerInfo">
+          <h3>{roomName}</h3>
+          <p>Last seen at...</p>
         </div>
-    )
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">John Yasis</span>
+          Hey Guys
+          <span className="chat__timestamp">3:69pm</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message..."
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message...
+          </button>
+        </form>
+        <MicNone />
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -21,7 +21,7 @@ function SidebarChat({ id, name, addNewChat }) {
     };
 
     return !addNewChat ? (
-        <Link to={`/rooms${id}`}>
+        <Link to={`/rooms/${id}`}>
             <div className='sidebarChat'>
             <Avatar src={`https://avatars.dicebear.com/api/pixel-art/:${seed}.svg`}/>
                 <div className="sidebarChat__info">
